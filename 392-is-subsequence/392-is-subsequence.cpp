e2742bfb97dd80426d3579db9510afbdbab046c2
Manoj Kumar <65900114+manojnsut@users.noreply.github.com>
2022-03-02 09:24:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -3,9 +3,9 @@ public:
     bool isSubsequence(string s, string t) {
         int i = 0, j = 0;
         int n = s.size(), m = t.size();
-        for(i = 0; i < m; i++)
+        for(i = 0; i < m; ++i)
               if(s[j] == t[i])
-                    j++;
+                    ++j;
         return n == j ? true : false;
         
     }
