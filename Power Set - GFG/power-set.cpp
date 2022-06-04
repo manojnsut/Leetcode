0c1522dc2525f37a433cf4361640599440547218
Manoj Kumar <65900114+manojnsut@users.noreply.github.com>
2022-06-04 15:29:12 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -7,20 +7,21 @@ class Solution{
 	public:
 		vector<string> AllPossibleStrings(string s){
 		    // Code here
-		    int len = s.size();
-		    vector<string> res;
-		    for(int cnt = 0; cnt < (1 << len); ++cnt) {
-		        string temp ="";
-		        for(int chars = 0; chars < len; chars++ ) {
-		             if( cnt & (1 << chars))
-		                 temp += s[chars];
-		        }
-		        if(temp.size())
-		            res.push_back(temp);
-		    }
-		    sort(res.begin(),res.end());
-		    return res;
-		}
+           vector<string> res;
+           int n = s.size();
+           for(int i = 0; i < (1 << n); ++i) {
+             string temp;
+             for(int j = 0; j < n; ++j) {
+                if( i & ( 1 << j)) 
+                   temp+=s[j];
+             }
+             if(temp.size())
+               res.push_back(temp);
+           }
+           sort(res.begin(),res.end());
+           return res;
+  
+   }
 };
 
 // { Driver Code Starts.
